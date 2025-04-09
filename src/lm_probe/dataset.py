af7cb23d5b3541396a8631f9fa227d343a6b70a0
Tyler Shoemaker <69219954+t-shoemaker@users.noreply.github.com>
2025-04-09 08:44:59 -0400

    Train/test split relies on Subset; data validation during initialization

--- src/lm_probe/dataset.py
+++ src/lm_probe/dataset.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset, random_split
+from torch.utils.data import Dataset, random_split, Subset
 
 
 class ProbeDataset(Dataset):
@@ -19,12 +19,23 @@ class ProbeDataset(Dataset):
             Token input IDs with shape (num_doc, seq_len)
         attention_mask torch.Tensor
             Attention mask for the tokens with shape (num_doc, seq_len)
-        labels : torch.Tensor
+        labels : torch.Tensor or list
             Labels
+
+        Raises
+        ------
+        ValueError
+            If the input IDs and attention mask shapes mismatch
         """
+        if input_ids.shape != attention_mask.shape:
+            raise ValueError(
+                f"Input IDs and attention mask shapes mismatch: "
+                f"got {input_ids.shape=} and {attention_mask.shape=}"
+            )
+
         self.input_ids = input_ids
         self.attention_mask = attention_mask
-        self.labels = labels
+        self.labels = torch.as_tensor(labels, dtype=torch.uint8)
 
     def __len__(self) -> int:
         """Dataset size."""
@@ -49,7 +60,7 @@ class ProbeDataset(Dataset):
             "labels": self.labels[idx],
         }
 
-    def tt_split(self, test_size: float = 0.2) -> tuple[Dataset, Dataset]:
+    def tt_split(self, test_size: float = 0.2) -> tuple[Subset, Subset]:
         """Do a random train/test split.
 
         Parameters
@@ -59,11 +70,11 @@ class ProbeDataset(Dataset):
 
         Returns
         -------
-        tuple[ProbeDataset]
+        tuple[Subset]
             Train/test datasets
         """
         test_size = round(test_size * len(self))
         train_size = len(self) - test_size
         train_set, test_set = random_split(self, [train_size, test_size])
 
-        return train_set.dataset, test_set.dataset
+        return train_set, test_set
